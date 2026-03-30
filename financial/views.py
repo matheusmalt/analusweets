67a262ea2b1dba6f7b31d98a30cee1df20150494
Matheus Malta <matheusmalta.pessoal@gmail.com>
2026-03-29 22:43:02 -0300

    Funções auxiliares movidas para utils.py

--- financial/views.py
+++ financial/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
-def dashboard(request):
-    return render(request, 'financial/pages/dashboard.html')
+from financial.utils import calculate_recipe
+# ===================================================================
+# View Django
+# ===================================================================
 
 def recipe_calc(request):
+    context = {
+        'results': {},
+        'form_data': {}
+    }
+
     if request.method == 'POST':
-        return render(request, 'financial/pages/recipe_calc.html')
+        results = calculate_recipe(request.POST)
+        context['results'] = results
+        context['form_data'] = dict(request.POST)  # para repopular o formulário
+
+    return render(request, 'financial/pages/recipe_calc.html', context)
+
+def dashboard(request):
+    return render(request, 'financial/pages/dashboard.html')
